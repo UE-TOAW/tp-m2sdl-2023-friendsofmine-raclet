9445420db5c789376c150389da169c86d2d828c7
raclet <raclet@irit.fr>
2023-10-13 05:11:18 +0200

    Resolved #13: controleur REST InscriptionController

---
+++ src/main/java/friendsofmine/m2/exceptions/InscriptionNotFoundException.java
@@ -0,0 +1,8 @@
+package friendsofmine.m2.exceptions;
+
+public class InscriptionNotFoundException extends RuntimeException {
+
+    public InscriptionNotFoundException(Long InscriptionId) {
+        super("Could not find exception" + InscriptionId + ".");
+    }
+}
