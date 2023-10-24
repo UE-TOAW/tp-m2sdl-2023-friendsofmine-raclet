5b76e50bef4ea231241cda250271296f6ed823a2
raclet <raclet@irit.fr>
2023-10-24 11:48:05 +0200

    Resolved #16: pagination

--- src/main/java/friendsofmine/m2/services/InscriptionService.java
+++ src/main/java/friendsofmine/m2/services/InscriptionService.java
@@ -3,9 +3,9 @@ package friendsofmine.m2.services;
 import friendsofmine.m2.domain.Inscription;
 import friendsofmine.m2.repositories.InscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class InscriptionService {
@@ -19,6 +19,14 @@ public class InscriptionService {
         return inscriptionRepository.save(inscription);
     }
 
+    public Page<Inscription> findAllInscription(Pageable pageable) {
+        return inscriptionRepository.findAll(pageable);
+    }
+
+    public Page<Inscription> findInscription(String nom, String titre, Pageable pageable) {
+        return inscriptionRepository.findByParticipantNomOrActiviteTitreAllIgnoreCase(nom, titre, pageable);
+    }
+
     public void deleteInscription(Long id) {
         inscriptionRepository.deleteById(id);
     }
@@ -29,17 +37,6 @@ public class InscriptionService {
 
     public long countInscription() {
         return inscriptionRepository.count();
-    }
-
-    public ArrayList<Inscription> findAllInscription() {
-        Iterable<Inscription> ins = inscriptionRepository.findAll();
-        ArrayList<Inscription> insList = new ArrayList<>();
-        ins.forEach(insList::add);
-        return insList;
-    }
-
-    public ArrayList<Inscription> findInscription(String nom, String titre) {
-        return inscriptionRepository.findByParticipantNomOrActiviteTitreAllIgnoreCase(nom, titre);
     }
 
     public InscriptionRepository getInscriptionRepository() {
